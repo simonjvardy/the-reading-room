2d3d10f4af4747b3bb31a243e039057cfd4bba6e
simonjvardy <sjvardy@gmail.com>
2021-02-19 19:52:57 +0000

    Dev: Add review form input fields

--- static/js/script.js
+++ static/js/script.js
@@ -4,4 +4,5 @@
   $(document).ready(function(){
     $('.sidenav').sidenav({edge: "right"});
     $('.collapsible').collapsible();
+    $('input#input_text, textarea#textarea2').characterCounter();
   });
