9583a0fe7e8540f3a7ecd0d93c005ad70df478b9
simonjvardy <sjvardy@gmail.com>
2021-03-12 10:09:26 +0000

    Dev: Remove app.py Debug=True before submission

--- app.py
+++ app.py
@@ -634,5 +634,4 @@ def service_unavailable(error):
 
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
-            port=int(os.environ.get("PORT")),
-            debug=True)
+            port=int(os.environ.get("PORT")))
