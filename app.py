208b1e8415142c85929ad7f927713fdee848496b
simonjvardy <sjvardy@gmail.com>
2021-03-12 10:04:19 +0000

    Fix: edit review genre bug. Genre field not displaying the current book genre.

--- app.py
+++ app.py
@@ -634,4 +634,5 @@ def service_unavailable(error):
 
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
-            port=int(os.environ.get("PORT")))
+            port=int(os.environ.get("PORT")),
+            debug=True)
